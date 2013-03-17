feb1d55feacac1af048b845c567516014cd2ae1e
Andrey Yegorov <ayegorov@groupon.com>
2013-03-16 21:53:44 -0700

    Deleted old files. Performed exercises for chapter 5.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,12 +6,22 @@ describe "Static pages" do
   
   subject { page }
   
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
+  
   describe "Home page" do
     before { visit root_path }
     
-    it { should have_selector('h1', text: 'Sample App') }
-    it { should have_selector('title', text: full_title('')) }
-    it { should_not have_selector('title', text: '| Home') }
+    let(:heading) {'Sample App' }
+    let(:page_title) { '' }
+    
+    it_should_behave_like "all static pages"
+    
+    #it { should have_selector('h1', text: 'Sample App') }
+    #it { should have_selector('title', text: full_title('')) }
+    #it { should_not have_selector('title', text: '| Home') }
     
   end
 
@@ -19,8 +29,13 @@ describe "Static pages" do
 
     before { visit help_path }
     
-    it { should have_selector('h1', text: 'Help') }
-    it { should have_selector('title', text: full_title('Help')) }
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
+    
+    it_should_behave_like "all static pages"
+    
+    # it { should have_selector('h1', text: 'Help') }
+    # it { should have_selector('title', text: full_title('Help')) }
     
   end
   
@@ -28,8 +43,13 @@ describe "Static pages" do
 
     before { visit about_path }
     
-    it { should have_selector('h1', :text => 'About Us') }
-    it { should have_selector('title', :text => full_title('About Us')) }
+    let(:heading) { 'About Us' }
+    let(:page_title) { 'About Us' }
+    
+    it_should_behave_like "all static pages"
+    
+    # it { should have_selector('h1', :text => 'About Us') }
+    # it { should have_selector('title', :text => full_title('About Us')) }
     
   end
   
@@ -37,9 +57,27 @@ describe "Static pages" do
 
     before {visit contact_path }
     
-    it { should have_selector('h1', :text => 'Contact') }
-    it { should have_selector('title', :text => full_title('Contact')) }
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact' }
+    
+    it_should_behave_like "all static pages"
+    
+    # it { should have_selector('h1', :text => 'Contact') }
+    # it { should have_selector('title', :text => full_title('Contact')) }
     
   end
+  
+  describe "Click test" do
+    it "should have something right here ... blah blah blah" do
+      visit root_path
+      click_link "About"
+      page.should have_selector 'title', :text => full_title('About Us')
+      click_link "Help"
+      page.should have_selector 'title', :text => full_title('Help')
+      click_link "Contact"
+      page.should have_selector 'title', text: full_title('Contact')
+    end
+  end
+  
 end
 
